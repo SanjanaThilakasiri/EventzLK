3a114dbea50415b91e1b4ee4f34d285386d9e2e7
SanjanaThilakasiri <160069278+SanjanaThilakasiri@users.noreply.github.com>
2025-09-15 14:06:17 +0530

    setup_routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Events from './pages/Events.jsx'
+import EventDetails from './pages/EventDetails.jsx'
+import Favorite from './pages/Favorite.jsx'
+import MyBookings from './pages/MyBookings.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
+const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
-    <div></div>
+    <>
+    <Toaster />
+    { !isAdminRoute && <Navbar /> }
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/events/:id" element={<EventDetails />} />
+      <Route path="/favorite" element={<Favorite />} />
+      <Route path="/my-bookings" element={<MyBookings />} />
+    </Routes>
+    {!isAdminRoute && <Footer />}
+    </>
   )
 }
 
